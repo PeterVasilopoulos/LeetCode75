3c0120a0c677f622eafa0d1fa2038fc150a57815
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-02-17 16:09:35 -0500

    updated solution

--- ProductOfAraryExceptSelf.js
+++ ProductOfAraryExceptSelf.js
@@ -1,37 +1,37 @@
-const nums = [1,2,3,4]
-
 var productExceptSelf = function(nums) {
-    // create a left array
+    // array of values starting at the left
     let leftArr = []
-    // create a left multiplication value
-    let leftMult = 1
 
-    // increment through nums
+    // loop through nums starting at front
     for(let i = 0; i < nums.length; i++) {
-        // set leftArr[i] = leftMult
-        leftArr[i] = leftMult
-        // multiply leftMult by the current nums value
-        leftMult *= nums[i]
+        // on first loop, add 1 to leftArr
+        if(i === 0) {
+            leftArr[i] = 1
+        } else {
+            // otherwise multiply num[i - 1] and leftArr[i - 1] to get current leftArr value
+            leftArr[i] = nums[i - 1] * leftArr[i - 1]
+        }
     }
 
-    // create a right array
+    // array of values starting at the right
     let rightArr = []
-    // create a right multiplication value
-    let rightMult = 1
 
-    // decrement through nums
+    // loop through nums starting at back
     for(let i = nums.length - 1; i >= 0; i--) {
-        // set rightArr[i] = rightMult
-        rightArr[i] = rightMult
-        // multiply rightMult by the current nums value
-        rightMult *= nums[i]
-        // multiply current rightArr value by leftArr value
-        rightArr[i] *= leftArr[i]
+        // on first loop, add 1 to rightArr
+        if(i === nums.length - 1) {
+            rightArr[i] = 1
+        } else {
+            // otherwise multiply num[i + 1] and rightArr[i + 1] to get current rightArr value
+            rightArr[i] = nums[i + 1] * rightArr[i + 1]
+        }
     }
 
-    console.log(leftArr)
-    console.log(rightArr)
+    // multiply related values in arrays and put into rightArr
+    for(let i = 0; i < rightArr.length; i++) {
+        rightArr[i] = leftArr[i] * rightArr[i]
+    }
+
+    // return rightArr
     return rightArr
 };
-
-productExceptSelf(nums)
