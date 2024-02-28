ab36bae43f7bd2c9accee78ba9f74c917acd846a
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-02-28 12:53:03 -0500

    add second better solution

--- SlidingWindow/MaximumAverageSubarray1.js
+++ SlidingWindow/MaximumAverageSubarray1.js
@@ -1,3 +1,36 @@
+var findMaxAverage2 = function(nums, k) {
+    // variable to hold temp average
+    let tempAvg = null
+
+    // calculate starting tempAvg value
+    for(let i = 0; i < k; i++) {
+        // add current value to tempAvg
+        tempAvg += nums[i]
+    }
+
+    // variable to hold the max average, 
+    // starting at tempAvg / k
+    let maxAvg = tempAvg / k
+
+    // loop through nums array
+    for(let i = k; i < nums.length; i++) {
+        // add current value to tempAvg
+        tempAvg += nums[i]
+        // remove old value from tempAvg
+        tempAvg -= nums[i - k]
+
+        // check if tempAvg / k is larger than maxAvg
+        if(tempAvg / k > maxAvg) {
+            // set new maxAvg
+            maxAvg = tempAvg / k
+        }
+    }
+
+    // return maxAvg
+    return maxAvg
+};
+
+
 var findMaxAverage = function(nums, k) {
     // variable to hold the max average
     let maxAvg = null
